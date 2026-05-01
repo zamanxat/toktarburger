9f5fd37c706cfe8ba4c1bc1432fee38ebc0e6db2
zamanxat <adileterkinovich17@icloud.com>
2026-05-01 09:00:53 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "tokburger-static-v50";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./admin.html",
+  "./tb-zhuristik-kiosk.html",
+  "./manifest.webmanifest",
+  "./logo.svg",
+  "./logo.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((response) => {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone)).catch(() => {});
+          return response;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
